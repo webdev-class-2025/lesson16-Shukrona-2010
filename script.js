bc0c06b1bd8fa74417f8f238d688865950904703
Shukrona Asanbekovna <shukronakarimdodova@gmail.com>
2025-08-25 16:25:08 +0500

    My homework

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+let role="user";
+let subscription = "pro";
+let productStatus ="sale";
 
+if(role==="admin"){
+    console.log(" Полный доступ");
+} else if(role==="user"){
+    console.log("Ограниченный доступ");
+} else if(role==="guest" ){
+    console.log("Только просмотр")
+}else {
+    console.log("Неизвестный пользователь");
+}
+
+
+switch(subscription){
+    case "free":
+        console.log("Минимум функций");
+    break;
+    case "pro":
+        console.log("Полный доступ");
+    break;
+    case "enterprise":
+        console.log("VIP-поддержка");
+    break;
+    default:
+        console.log("Неизвестный тариф");
+}
+
+if (productStatus ==="available" && (subscription==="pro"|| subscription==="enterprise")){
+    console.log("Можно заказать");
+}else if(productStatus==="sale" && (role==="user"||role==="guest")){
+    console.log("Доступна акция");
+}else if(productStatus==="out-of-stock"){
+    console.log("Товар недоступен");
+
+}
